413ab0b9d352516016dd86c6679babcaaa592f13
saris20038 <saris20038@gmail.com>
2021-02-14 23:25:11 -0500

    Clase 3

---
+++ clases/CLASE2.py
@@ -0,0 +1,23 @@
+pruebaV = True
+pruebaF = False
+print(pruebaF)
+print(pruebaV)
+edad = 18
+estatura = 1.67
+peso = 62
+NOMBRE = "Sara Nieto"
+print("########Mayor Edad#######")
+isMayorEdad = edad >= 18
+print(isMayorEdad)
+print("####Supera la Estatura Promedio####")
+isMayorEstatura = estatura >= 1.71
+print(isMayorEstatura)
+print("###Peso Diferente a 62###")
+isDiferentePeso = peso != 62 
+print(isDiferentePeso)
+print("###Apellido está en el nombre?###")
+apellido = "Nieto"
+isApellido= apellido in NOMBRE
+print(isApellido)
+
+
---
+++ clases/operaciones.py
@@ -0,0 +1,12 @@
+numeroA = 12
+numeroB = 23
+sumar = numeroA + numeroB
+restar = numeroA - numeroB
+print (f"el resultado de la suma da {sumar}")
+print(f"la resta da {restar}")
+multiplicar = numeroA * numeroB
+print(f"resultado de multiplicación es {multiplicar}")
+dividir = numeroA / numeroB
+print(f"el resultado de la division es {dividir}")
+exponente = numeroA ** numeroB
+print(f"resultado de 12 elevado a la 23 es {exponente}")
