ef7fe50b380fdf3591cb4ccd8743dd808542b5dc
saris20038 <saris20038@gmail.com>
2021-03-15 19:22:43 -0500

    practice

--- clases/while.py
+++ clases/while.py
@@ -3,7 +3,8 @@ Preguntadinero = "¿Cuanta plata tiene ahorrada?: "
 Preguntaprecio = "¿Cuanto vale el telefono?: "
 plataQueTengo = float(input(Preguntadinero))
 PrecioTelefono = float(input(Preguntaprecio))
-
+print(plataQueTengo)
+print(PrecioTelefono)
 while(plataQueTengo < PrecioTelefono):
     print("No te alcanza, sigue ahorrando")
 (plataQueTengo>PrecioTelefono)
