7ce67e3e67512c39f9df56465721502b990bfaa5
saris20038 <saris20038@gmail.com>
2021-04-22 06:48:42 -0500

    jaja

--- TALLERES/TALLERCLASE.py
+++ TALLERES/TALLERCLASE.py
@@ -1,4 +1,5 @@
 #FIRST POINT 
+print("#"*80)
 class Torta ():
     '''Resposteria fina a tu gusto
     '''
@@ -16,6 +17,7 @@ torta1 = Torta("fresa", "cuadrada", "40 cm")
 torta1.mostrarAtributos()
 
 #SECOND POINT 
+print("#"*80)
 class EstudianteUniversidad ():
     def __init__ (self, edadEntrada, nombreEntrada, idEntrada, carreraEntrada, semestreEntrada):
         print ("Hi, Im a student")
@@ -30,3 +32,30 @@ class EstudianteUniversidad ():
 
 Student1 = EstudianteUniversidad("22", "Catalina Mesa", "505068", "Medicina", "5") 
 Student1.RepasoEspecifico("Histología", "3")
+
+#THIRD POINT 
+print("#"*80)
+class Nutricionista ():
+    def __init__ (self, edadEntrada, nombreEntrada,universidadEntrada):
+        print ("Hi, Im a student")
+        self.edad = edadEntrada
+        self.nombre = nombreEntrada
+        self.universidad = universidadEntrada
+        print (f"Me llamo {self.nombre} , estudio en {self.universidad}")
+    def IMC(self, PesoEntrada, alturaEnttrada):
+        IMC= PesoEntrada / (alturaEnttrada**2)
+        print (f" Su IMC {IMC}")
+
+Nutricionista1 = Nutricionista("21", "Susana Ramirez", "Universidad de Antioquia")
+Nutricionista1.IMC(2 , 1.78)
+
+#FOURTH POINT 
+print("#"*80)
+class Kangaroo():
+    def __init__(self, nameInput, idInput, edadInput):
+        print("Hi, Im a Kangaroo ")
+        self.name = nameInput
+        self.specie = "Human"
+        self.name = nameInput 
+        self.height = heightInput
+        self.money = 0
