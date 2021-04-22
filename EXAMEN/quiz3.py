128ed2e683eb557bd5e3389acf3588230dca5322
saris20038 <saris20038@gmail.com>
2021-04-22 07:50:06 -0500

    jj

--- EXAMEN/quiz3.py
+++ EXAMEN/quiz3.py
@@ -16,3 +16,17 @@ class Usuario ():
 Usuario1 = Usuario("21", "Alexa", "mujer", "Española")
 Usuario1.mostrarAtributos()
 Usuario1.escucharCancion("La flaca")
+
+
+class Artista (Usuario):
+    def __init__(self, generoEntrada, numerocancionesEntrada, albumsEntrada, edadEntrada, nombreEntrada, sexoEntrada, nacionalidadEntrada):
+        Usuario.__init__(self, edadEntrada, nombreEntrada, sexoEntrada, nacionalidadEntrada )
+        self.generomusical = generoEntrada
+        self.numerocanciones = numerocancionesEntrada
+        self.numeroDeAlbums = albumsEntrada
+    def asistirConcierto(self, localidadEntrada):
+        self.localidadconcert = localidadEntrada
+        print (f" Hola, soy {self.nombre}, iré a un concierto en {self.localidadconcert} del genero {self.generomusical} que tiene un total de albums de {self.numeroDeAlbums} con {self.numerocanciones} canciones. ¡QUE EMOCIÓN!")
+
+Fabio= Artista("Country", "30", "22", "67", "Fabio", "hombre", "Colombiano")
+Fabio.asistirConcierto("Estados Unidos")
